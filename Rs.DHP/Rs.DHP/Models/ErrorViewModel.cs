1bcc8a0341e670c4a651770d496a3c5f9caaf2f7
Rince King <xhp681@outlook.com>
2018-10-11 10:40:24 +0800

    the second submit

---
+++ Rs.DHP/Rs.DHP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rs.DHP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
