882179816987e1aa4420d1d4199c410ca1453698
Rince King <xhp681@outlook.com>
2018-10-29 09:51:45 +0800

    submit in 2018-10-29

---
+++ Rs.DHP/Rs.DCV/interface/IDatabaseConfiguring.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Rs.DCV
+{
+    public interface IDatabaseConfiguring
+    {
+        void OnConfiguring(DbContextOptionsBuilder optionsBuilder, DbConnection dbConnectionForReusing);
+    }
+}
